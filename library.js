d414766403e6ceaec20b2631b83971269de99a39
Carlos Valdez <valdezcarlos.cs@gmail.com>
2023-10-17 11:58:45 -0600

    added a dialog modal to index.html/styles.css/and changes to library.js to eventListen to a new book being added, currently just the initial setup

--- library.js
+++ library.js
@@ -1,1 +1,32 @@
 // library.js by Carlos A. Valdez
+
+/* 
+    For Button and dialog modal form
+*/
+const openButton = document.getElementById("addBookBtn");
+const theModal = document.getElementById("addBookModal")
+const closeButton = document.getElementById("closeBtn");
+
+openButton.addEventListener("click", () => {
+    theModal.showModal();
+})
+
+closeButton.addEventListener("click", () => {
+    theModal.close();
+})
+
+
+
+const myLibrary = [];
+
+// debating if we can make a seperate class for Books and a seperate class for Libraries, shall see..
+function Book(title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+}
+
+function addBookToLibrary() {
+    // do stuff..
+}
